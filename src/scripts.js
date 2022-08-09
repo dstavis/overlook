6336acf0ac4ecb3267bf8c07e629cb284d927be1
David Stavis <davidstavis@gmail.com>
2022-08-09 07:40:59 -0600

    Display available rooms when a date is chosen

--- src/scripts.js
+++ src/scripts.js
@@ -21,6 +21,8 @@ let roomsURL = "http://localhost:3001/api/v1/rooms"
 
 let currentCustomer;
 let today = new Date().toLocaleDateString("en-CA")
+let rooms;
+let bookings;
 
 // Query Selectors
 
@@ -32,9 +34,13 @@ const customerTotalSpentDisplay = document.querySelector(".total-spent .dollars"
 const individualBookingTemplate = document.querySelector(".individual-booking.template")
 const individualBookingsContainer = document.querySelector(".individual-bookings-container")
 
+const individualRoomDetailTemplate = document.querySelector(".individual-room-detail.template")
+const individualRoomDetailsContainer = document.querySelector(".individual-room-details-container")
+
 // Event listeners
 
 window.addEventListener("load", start)
+dateControl.addEventListener("input", dateChanged)
 
 function chooseRandomCustomerID() {
   let randomIDNumber = Math.floor((1 + Math.random() * 50))
@@ -62,21 +68,61 @@ function loadData(){
       let roomsData = data[2].rooms
       let customerData = data[3]
       
-      let apiData = {bookings: bookingsData, rooms: roomsData, customer: customerData}
+      let apiData = { bookings: bookingsData, rooms: roomsData, customer: customerData }
       
+      // hold on to the rooms 
+      rooms = apiData.rooms;
+      bookings = apiData.bookings;
+
       // massage the data into convenient shapes for display scripts
       massageData(apiData)
       // display scripts
       displayCustomerName(currentCustomer)
       displayBookingsForCustomer(currentCustomer)
     })
-    
+}
+
+function dateChanged(event) {
+  let newDate = event.target.value;
+  // check all the rooms.
+  // for each room's room number, are there any bookings with that room number whose date matches newDate?
+  // if so, that room is OUT
+  // all the other rooms are in
+  console.log("wat")
+  let roomsForDate = rooms.filter( (room) => {
+    return !bookings.some( (booking) => {
+      return ((booking.roomNumber === room.number) && (booking.date === newDate.split("-").join("/")))
+    })
+  })
+  // show a list of all the rooms that made it through the filter 
+  showRoomsForReservation(roomsForDate)
+}
+
+function showRoomsForReservation(roomDetails) {
+  // empty the roomsContainer of any old rooms!!!
+  individualRoomDetailsContainer.replaceChildren()
+  // for each roomDetail, do the following
+  roomDetails.forEach( (roomDetail) => {
+    // grab the template for an individualRoomDetail and clone it, then remove the template and hidden classes
+    let freshRoomDisplay = individualRoomDetailTemplate.cloneNode(true)
+    freshRoomDisplay.classList.remove("template")
+    freshRoomDisplay.classList.remove("hidden")
+    // fill the fresh individualRoomDetail with the data extracted from the room
+    freshRoomDisplay.querySelector(".room-number").innerText = roomDetail.number
+    freshRoomDisplay.querySelector(".room-cost").innerText = "$" + roomDetail.costPerNight
+    freshRoomDisplay.querySelector(".beds").innerText = roomDetail.numBeds + " " + roomDetail.bedSize + "(s)"
+    let hasBidet = roomDetail.bidet ? "Yes" : "No"
+    freshRoomDisplay.querySelector(".bidet").innerText = "Bidet: " + hasBidet
+    // find the container we want the individualRoomDetail(s) to be displayed in
+    // append the filled-in individualRoomDetail to the container
+    individualRoomDetailsContainer.append(freshRoomDisplay)
+  })
 }
   
 function massageData(apiData) {
   // What do I want to know for display?
   // I want to know all the bookings made by a single customer.
-  
+
   // find bookings whose userID field matches customer.ID field
   let customerBookingsWithPrice = apiData.bookings.filter( (booking) => {
     return booking.userID === apiData.customer.id
@@ -118,13 +164,7 @@ function displayBookingsForCustomer(customer) {
   // Now that I've got that info, what do I want to do with it?
   // I want to display the customer's total spent in the header
   customerTotalSpentDisplay.innerText = new Intl.NumberFormat('en-US', { style: "currency", currency: "USD"}).format(customer.totalSpent)
-  console.log("~~~~do da thing")
 
-  // query select the container for the rows of bookings
-  
-  // make a new booking from the template
-
-  
   customer.bookings.forEach( (booking) => {
     displayBooking(booking)
   } )
