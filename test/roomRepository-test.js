ff0dc201cfec0b98be9122f0c6b7b8d8d7ee4ec7
David Stavis <davidstavis@gmail.com>
2022-08-10 20:05:30 -0600

    Test room filter function

--- test/roomRepository-test.js
+++ test/roomRepository-test.js
@@ -13,8 +13,8 @@ let bookingsData;
 describe('RoomRepository', function() {
   beforeEach(function() {
 
-    roomsData = JSON.parse('{"rooms":[{"number":1,"roomType":"residential suite","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":358.4},{"number":2,"roomType":"suite","bidet":false,"bedSize":"full","numBeds":2,"costPerNight":477.38},{"number":3,"roomType":"single room","bidet":false,"bedSize":"king","numBeds":1,"costPerNight":491.14},{"number":4,"roomType":"single room","bidet":false,"bedSize":"queen","numBeds":1,"costPerNight":429.44},{"number":5,"roomType":"single room","bidet":true,"bedSize":"queen","numBeds":2,"costPerNight":340.17}]}')
-    bookingsData = JSON.parse('{"bookings":[{"id":"5fwrgu4i7k55hl6sz","userID":9,"date":"2022/04/22","roomNumber":1},{"id":"5fwrgu4i7k55hl6t5","userID":43,"date":"2022/01/24","roomNumber":2},{"id":"5fwrgu4i7k55hl6t6","userID":13,"date":"2022/01/10","roomNumber":3},{"id":"5fwrgu4i7k55hl6t7","userID":20,"date":"2022/02/16","roomNumber":4},{"id":"5fwrgu4i7k55hl6t8","userID":1,"date":"2022/02/05","roomNumber":5}]}' )
+    roomsData = JSON.parse('{"rooms":[{"number":1,"roomType":"single room","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":358.4},{"number":2,"roomType":"suite","bidet":false,"bedSize":"full","numBeds":2,"costPerNight":477.38},{"number":3,"roomType":"junior suite","bidet":false,"bedSize":"king","numBeds":1,"costPerNight":491.14},{"number":4,"roomType":"residential suite","bidet":false,"bedSize":"queen","numBeds":1,"costPerNight":429.44},{"number":5,"roomType":"single room","bidet":true,"bedSize":"queen","numBeds":2,"costPerNight":340.17}]}')
+    bookingsData = JSON.parse('{"bookings":[{"id":"5fwrgu4i7k55hl6sz","userID":9,"date":"2022/08/10","roomNumber":1},{"id":"5fwrgu4i7k55hl6t5","userID":43,"date":"2022/08/10","roomNumber":5},{"id":"5fwrgu4i7k55hl6t6","userID":13,"date":"2022/08/10","roomNumber":3},{"id":"5fwrgu4i7k55hl6t7","userID":20,"date":"2022/08/10","roomNumber":4},{"id":"5fwrgu4i7k55hl6t8","userID":1,"date":"2022/08/10","roomNumber":5}]}' )
     
     rooms = roomsData.rooms
     bookings = bookingsData.bookings.map( (bookingData) => {return new Booking (bookingData)})
@@ -34,7 +34,7 @@ describe('RoomRepository', function() {
   it('should be able to check if a room is available', function() {
     // happy path, there is no booking for that room on this date
     let room = rooms[0];
-    let date = "2022/04/23";
+    let date = "2022/08/12";
     
     expect(roomRepository.notBooked(room, date)).to.equal(true);
   });
@@ -42,27 +42,29 @@ describe('RoomRepository', function() {
   it('should be able to check if a room is already booked', function() {
     // sad path, there is at least one booking for this room on this date
     let room = rooms[0];
-    let date = "2022/04/22";
+    let date = "2022/08/10";
     
     expect(roomRepository.notBooked(room, date)).to.equal(false);
   });
 
-  it.skip('should return unbooked rooms that match both the date and the roomType chosen', function() {
+  it('should return unbooked rooms that match both the date and the roomType chosen', function() {
     // happy path
     // we spit out unbooked rooms that match both the date and the roomType chosen
-    let date = ""
-    let roomType = ""
+    // there are two unbooked rooms 
+    let date = "2022/08/11"
+    let roomType = "single room"
 
     expect(roomRepository.filterRooms(date, roomType).length).to.equal(2);
   });
+  
 
-  it.skip('should return an empty array if no rooms match', function() {
+  it('should return an empty array if no rooms match', function() {
     // sad path
     // there are no unbooked rooms that match both the date and the roomType chosen, so we...
       // return an empty array and let scripts.js figure out to show the error?
       // return an error message?
-    let roomType = rooms[0];
-    let date = "2022/04/22";
+    let date = "2022/08/10";
+    let roomType = "junior suite"
 
     expect(roomRepository.filterRooms(date, roomType).length).to.equal(0);
   });
